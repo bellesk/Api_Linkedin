ce2bed785b53ad3b1adb7f36ea3081181a3066ad
bellesk <killian.bellesoeur@epitech.eu>
2016-04-28 02:32:30 +0200

    first commit

---
+++ Home2.php
@@ -0,0 +1,142 @@
+<!DOCTYPE HTML>
+<!--
+	Astral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Astral by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="Result/assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="Result/assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="Result/assets/css/noscript.css" /></noscript>
+    <!--[if lte IE 8]><link rel="stylesheet" href="Result/assets/css/ie8.css" /><![endif]-->
+</head>
+<body>
+
+<!-- Wrapper-->
+<div id="wrapper">
+
+    <!-- Nav -->
+    <nav id="nav">
+        <a href="#me" class="icon fa-home active"><span>Home</span></a>
+        <a href="#work" class="icon fa-folder"><span>Work</span></a>
+        <a href="#contact" class="icon fa-envelope"><span>Contact</span></a>
+        <a href="#" class="icon fa-twitter"><span>Twitter</span></a>
+    </nav>
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- Me -->
+        <article id="me" class="panel">
+            <header>
+                <h1 align="center">
+                        Log has linkedin, click on connect
+                </h1>
+                <p>
+                    <form action="connect.php" method="post">
+                    <p align="center">
+                        <input type="submit" value="Connect" />
+                    </p>
+                    </form>
+                </p>
+            </header>
+            <a href="#work" class="jumplink pic">
+                <span class="arrow icon fa-chevron-right"><span>See my work</span></span>
+                <img src="images/me.jpg" alt="" />
+            </a>
+        </article>
+
+        <!-- Work -->
+        <article id="work" class="panel">
+            <header>
+                <h2>Profil</h2>
+            </header>
+            <p>
+                Les différentes informations sur la personne telle que twitter, git, numéro de téléphone etc.
+            </p>
+            <section>
+                <div class="row">
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic01.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic02.jpg" alt=""></a>
+                    </div>
+                    <div class="4u$ 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic03.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic04.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic05.jpg" alt=""></a>
+                    </div>
+                    <div class="4u$ 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic06.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic07.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic08.jpg" alt=""></a>
+                    </div>
+                    <div class="4u$ 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic09.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic10.jpg" alt=""></a>
+                    </div>
+                    <div class="4u 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic11.jpg" alt=""></a>
+                    </div>
+                    <div class="4u$ 12u$(mobile)">
+                        <a href="#" class="image fit"><img src="Result/images/pic12.jpg" alt=""></a>
+                    </div>
+                </div>
+            </section>
+        </article>
+
+        <!-- Contact -->
+        <article id="contact" class="panel">
+            <header>
+                <h2>Contact Me</h2>
+            </header>
+            <form action="#" method="post">
+                <div>
+                    <div class="row">
+                        <div class="6u 12u$(mobile)">
+                            <input type="text" name="name" placeholder="Name" />
+                        </div>
+                        <div class="6u$ 12u$(mobile)">
+                            <input type="text" name="email" placeholder="Email" />
+                        </div>
+                        <div class="12u$">
+                            <input type="text" name="subject" placeholder="Subject" />
+                        </div>
+                        <div class="12u$">
+                            <textarea name="message" placeholder="Message" rows="8"></textarea>
+                        </div>
+                        <div class="12u$">
+                            <input type="submit" value="Send Message" />
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </article>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="Result/assets/js/jquery.min.js"></script>
+<script src="Result/assets/js/skel.min.js"></script>
+<script src="Result/assets/js/skel-viewport.min.js"></script>
+<script src="Result/assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="Result/assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="Result/assets/js/main.js"></script>
+
+</body>
+</html>
---
+++ connect.php
@@ -0,0 +1,117 @@
+<?php
+
+//$config = include(dirname(__FILE__).'/config.php');
+// Change these
+define('API_KEY', '772po0wxx2l5hl');
+define('API_SECRET', 'qx8GyFEG9DsI0Y87');
+define('REDIRECT_URI', 'http://localhost:63342/Linkedin/connect.php');
+define('SCOPE', 'r_basicprofile r_emailaddress rw_company_admin w_share');
+
+// You'll probably use a database
+session_name('linkedin');
+session_start();
+
+// OAuth 2 Control Flow
+if (isset($_GET['error'])) {
+    // LinkedIn returned an error
+    print $_GET['error'] . ': ' . $_GET['error_description'];
+    exit;
+} elseif (isset($_GET['code'])) {
+    // User authorized your application
+    if ($_SESSION['state'] == $_GET['state']) {
+        // Get token so you can make API calls
+        getAccessToken();
+    } else {
+        // CSRF attack? Or did you mix up your states?
+        exit;
+    }
+} else {
+    if ((empty($_SESSION['expires_at'])) || (time() > $_SESSION['expires_at'])) {
+        // Token has expired, clear the state
+        $_SESSION = array();
+    }
+    if (empty($_SESSION['access_token'])) {
+        // Start authorization process
+        getAuthorizationCode();
+    }
+}
+
+// Congratulations! You have a valid token. Now fetch your profile
+$user = fetch('GET', '/v1/people/~');
+$send_url = "Result/index2.php?firstName=" .urlencode($user->firstName) ."&lastName=" .urlencode($user->lastName) ."&Headline=" .urlencode($user->headline) ."&email=" .urlencode($user->email-address);
+header("Location: $send_url");
+
+function getAuthorizationCode() {
+    $params = array('response_type' => 'code',
+        'client_id' => API_KEY,
+        'scope' => SCOPE,
+        'state' => uniqid('', true), // unique long string
+        'redirect_uri' => REDIRECT_URI,
+    );
+
+    // Authentication request
+    $url = 'https://www.linkedin.com/uas/oauth2/authorization?' . http_build_query($params);
+
+
+    // Needed to identify request when it returns to us
+    $_SESSION['state'] = $params['state'];
+
+    // Redirect user to authenticate
+    
+    header("Location: $url");
+    exit;
+}
+
+function getAccessToken() {
+    $params = array('grant_type' => 'authorization_code',
+        'client_id' => API_KEY,
+        'client_secret' => API_SECRET,
+        'code' => $_GET['code'],
+        'redirect_uri' => REDIRECT_URI,
+    );
+
+    // Access Token request
+    $url = 'https://www.linkedin.com/uas/oauth2/accessToken?' . http_build_query($params);
+
+    // Tell streams to make a POST request
+    $context = stream_context_create(
+        array('http' =>
+            array('method' => 'POST',
+            )
+        )
+    );
+    // Retrieve access token information
+    $response = file_get_contents($url, false, $context);
+
+    // Native PHP object, please
+    $token = json_decode($response);
+
+    // Store access token and expiration time
+    $_SESSION['access_token'] = $token->access_token; // guard this!
+    $_SESSION['expires_in']   = $token->expires_in; // relative time (in seconds)
+    $_SESSION['expires_at']   = time() + $_SESSION['expires_in']; // absolute time
+    return true;
+}
+
+function fetch($method, $resource)
+{
+//    header("Location: https://www.google.fr");
+    $params = array('oauth2_access_token' => $_SESSION['access_token'],
+        'format' => 'json',);
+
+    // Need to use HTTPS
+    $url = 'https://api.linkedin.com' . $resource . '?' . http_build_query($params);
+    // Tell streams to make a (GET, POST, PUT, or DELETE) request
+    $context = stream_context_create(
+        array('http' =>
+            array('method' => $method,
+            )
+        )
+    );
+    // Hocus Pocus
+    $response = file_get_contents($url, false, $context);
+    json_decode($response);
+    // Native PHP object, please
+    return json_decode($response);
+}
+?>
